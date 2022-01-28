1811a7834d7894a615724e417e0cffbe2ac4f2ca
jana0912 <jana.pinilla@outlook.com>
2022-01-28 13:41:06 -0500

    Cambios 2

--- Complementaria1.R
+++ Complementaria1.R
@@ -16,3 +16,25 @@ uspop
 uspop[5]
 uspop[c(5,8)]
 uspop[c(5:8)]
+
+
+###PARTE 2
+
+
+#ejercicio
+
+x<-c(2,4,6,8,10)
+y<-x[c(2,4)]
+
+data("iris")
+
+
+
+
+iris$Sepal.Length
+iris[,5]
+iris[1,5]
+
+Largo_petalo<-iris$Sepal.Length
+
+media_largo<-mean(Largo_petalo[1:50])
